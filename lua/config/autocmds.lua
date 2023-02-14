433d529c1b2b0a8d257fd15c92a910a236458ef8
Amaan Qureshi <amaanq12@gmail.com>
2023-02-13 22:15:59 -0500

    feat(autocmds): quit dap windows with q

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+--  ╭──────────────────────────────────────────────────────────────────────────────╮
+--  │           Autocmds are automatically loaded on the VeryLazy event            │
+--  │                    Default autocmds that are always set:                     │
+--  │ https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua │
+--  ╰──────────────────────────────────────────────────────────────────────────────╯
 
 -- show cursor line only in active window
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
@@ -65,6 +67,32 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- Tree-Sitter highlighting for filetypes not autodetected
 vim.filetype.add({
 	extension = {},
+})
+
+-- close dap-float with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"dap-float",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true, desc = "Close Dap Float" })
+	end,
+})
+
+-- close dap-terminal with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"dap-terminal",
+	},
+	callback = function(event)
+		vim.keymap.set(
+			"n",
+			"q",
+			"<cmd>bdelete!<cr>",
+			{ buffer = event.buf, silent = true, desc = "Close Dap Terminal" }
+		)
+	end,
 })
 
 -- Disable diagnostics in a .env file
