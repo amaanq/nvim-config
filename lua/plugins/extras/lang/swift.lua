4814bf6cdff6d3d8f82c71ba1a3aeb69fba460c1
Amaan Qureshi <amaanq12@gmail.com>
2023-02-13 22:14:01 -0500

    feat: add swift lang settings (I won't use this likely)

---
+++ lua/plugins/extras/lang/swift.lua
@@ -0,0 +1,26 @@
+return {
+
+	-- add swift to treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "swift" })
+			end
+		end,
+	},
+
+	-- correctly setup lspconfig for Swift
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			-- make sure mason installs the server
+			servers = {
+				sourcekit = {},
+			},
+			setup = {
+				sourcekit = function(_, opts) end,
+			},
+		},
+	},
+}
