d4df8e49cdcbd14636757b72b58654fbe54f978c
Amaan Qureshi <amaanq12@gmail.com>
2023-02-18 12:23:37 -0500

    chore: refactor toggleterm config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -319,28 +319,20 @@ return {
 	{
 		"akinsho/nvim-toggleterm.lua",
 		keys = "<C-`>",
-		event = "BufReadPre",
-		config = function()
-			require("toggleterm").setup({
-				size = 20,
-				hide_numbers = true,
-				open_mapping = [[<C-`>]],
-				shade_filetypes = {},
-				shade_terminals = true,
-				shading_factor = 0.3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-				start_in_insert = true,
-				persist_size = true,
-				direction = "float",
-				close_on_exit = true,
-				shell = vim.o.shell,
-			})
-
+		event = "VeryLazy",
+		init = function()
 			-- Hide number column for
 			-- vim.cmd [[au TermOpen * setlocal nonumber norelativenumber]]
 
 			-- Esc twice to get to normal mode
 			vim.cmd([[tnoremap <ESC> <C-\><C-N>]])
 		end,
+		opts = {
+			size = 20,
+			open_mapping = [[<C-`>]],
+			shading_factor = 0.3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+			direction = "float",
+		},
 	},
 
 	-- git blame
