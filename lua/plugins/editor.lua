711c093fa60d230a551a35c69e18aeb05ec8e992
Amaan Qureshi <amaanq12@gmail.com>
2023-02-18 03:42:38 -0500

    feat: add neo-tree diagnostics

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -18,6 +18,23 @@ return {
 	-- file explorer
 	{
 		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"mrbjarksen/neo-tree-diagnostics.nvim",
+			{
+				"s1n7ax/nvim-window-picker",
+				opts = {
+					use_winbar = "smart",
+					autoselect_one = true,
+					include_current = false,
+					filter_rules = {
+						bo = {
+							filetype = { "neo-tree-popup", "quickfix" },
+							buftype = { "terminal", "quickfix", "nofile" },
+						},
+					},
+				},
+			},
+		},
 		opts = function(_, opts)
 			opts.close_if_last_window = true -- Close Neo-tree if it is the last window left in the tab
 			opts.group_empty_dirs = true -- When true, empty folders will be grouped together
