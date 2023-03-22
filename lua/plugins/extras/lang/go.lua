6b81cb0cea6e10e33ff59afa06f825c821ebde1f
Amaan Qureshi <amaanq12@gmail.com>
2023-03-22 03:14:47 -0400

    feat(lsp): fix go settings, semantic highlights now work

--- lua/plugins/extras/lang/go.lua
+++ lua/plugins/extras/lang/go.lua
@@ -57,42 +57,62 @@ return {
 				golangci_lint_ls = {},
 				gopls = {},
 			},
-			settings = {
-				golangci_lint_ls = {},
-				gopls = {
-					gofumpt = true,
-					codelenses = {
-						gc_details = false,
-						generate = true,
-						regenerate_cgo = true,
-						run_govulncheck = true,
-						test = true,
-						tidy = true,
-						upgrade_dependency = true,
-						vendor = true,
-					},
-					hints = {
-						assignVariableTypes = true,
-						compositeLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					},
-					analyses = {
-						fieldalignment = true,
-						nilness = true,
-						unusedparams = true,
-						unusedwrite = true,
-						useany = true,
-					},
-					usePlaceholders = true,
-					completeUnimported = true,
-					staticcheck = true,
-					directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-					semanticTokens = true,
-				},
+			setup = {
+				gopls = function(_, opts)
+					require("lazyvim.util").on_attach(function(client, _)
+						if client.name == "gopls" then
+							-- workaround for gopls not supporting semanticTokensProvider
+							-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+							if not client.server_capabilities.semanticTokensProvider then
+								local semanticTokens = client.config.capabilities.textDocument.semanticTokens
+								client.server_capabilities.semanticTokensProvider = {
+									full = true,
+									legend = {
+										tokenTypes = semanticTokens.tokenTypes,
+										tokenModifiers = semanticTokens.tokenModifiers,
+									},
+									range = true,
+								}
+							end
+						end
+					end)
+					opts.settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								fieldalignment = true,
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					}
+				end,
 			},
 		},
 	},
