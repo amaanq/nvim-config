d3d53b48f1210068fc991ac6162f6cb37bc86a38
Amaan Qureshi <amaanq12@gmail.com>
2023-04-22 21:54:36 -0400

    fix(csharp): actually correct semantic tokens

--- lua/plugins/extras/lang/csharp.lua
+++ lua/plugins/extras/lang/csharp.lua
@@ -23,17 +23,38 @@ return {
       -- configure omnisharp to fix the semantic tokens bug (really annoying)
       setup = {
         omnisharp = function(_, _)
-          require("lazyvim.util").on_attach(function(client, _)
+          ---@param input string
+          ---@return string
+          local function toCamelCase(input)
+            local words = {} ---@type string[]
+            local sanitized_input = input:gsub("[^%w%s]", " ")
+            for word in sanitized_input:gmatch("%S+") do
+              table.insert(words, word:lower())
+            end
+
+            for i = 2, #words do
+              words[i] = words[i]:gsub("^%l", string.upper)
+            end
+
+            return table.concat(words)
+          end
+
+          require("lazyvim.util").on_attach(function(client, _) ---@param client lspconfig.options.omnisharp
             if client.name == "omnisharp" then
-              ---@type string[]
-              local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+              local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers ---@type string[]
               for i, v in ipairs(tokenModifiers) do
-                tokenModifiers[i] = v:gsub(" ", "_")
+                if v:match(" name$") then
+                  tokenModifiers[i] = v:gsub(" name$", "")
+                end
+                tokenModifiers[i] = toCamelCase(tokenModifiers[i])
               end
-              ---@type string[]
-              local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+
+              local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes ---@type string[]
               for i, v in ipairs(tokenTypes) do
-                tokenTypes[i] = v:gsub(" ", "_")
+                if v:match(" name$") then
+                  tokenTypes[i] = v:gsub(" name$", "")
+                end
+                tokenTypes[i] = toCamelCase(tokenTypes[i])
               end
             end
           end)
