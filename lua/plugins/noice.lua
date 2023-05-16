cb8611f6aae48f3be2f958ae9b71936c784fc9c4
Amaan Qureshi <amaanq12@gmail.com>
2023-05-15 23:12:33 -0400

    fix: restore some noice opts

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -33,6 +33,46 @@ return {
       },
     }
 
+    opts.views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    }
+
+    opts.presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = true,
+      cmdline_output_to_split = false,
+      lsp_doc_border = true,
+    }
+
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "markdown",
       callback = function(event)
