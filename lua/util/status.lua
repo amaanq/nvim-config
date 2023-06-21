16db650bee340432f86f0636a04ec99ff8789733
Amaan Qureshi <amaanq12@gmail.com>
2023-06-21 03:01:56 -0400

    fix(status): dont show signcolumn when set to no

--- lua/util/status.lua
+++ lua/util/status.lua
@@ -2,30 +2,32 @@ local M = {}
 _G.Status = M
 
 ---@return {name:string, text:string, texthl:string}[]
-function M.get_signs()
-  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+function M.get_signs(win)
+  local buf = vim.api.nvim_win_get_buf(win)
   return vim.tbl_map(function(sign) --- @param sign {name:string, text:string, texthl:string}
     return vim.fn.sign_getdefined(sign.name)[1]
   end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
 end
 
 function M.column()
+  local win = vim.g.statusline_winid
+  if vim.wo[win].signcolumn == "no" then
+    return ""
+  end
   local sign, git_sign ---@type {name:string, text:string, texthl:string}, {name:string, text:string, texthl:string}
-  for _, s in ipairs(M.get_signs()) do
+  for _, s in ipairs(M.get_signs(win)) do
     if s.name:find("GitSign") then
       git_sign = s
     else
       sign = s
     end
   end
-
   local nu = " "
-  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, "number")
-  if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
+  if vim.wo[win].number and vim.wo[win].relativenumber and vim.v.virtnum == 0 then
     nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
   end
   local components = {
-    sign and ("%#" .. (sign.texthl or "DiagnosticInfo") .. "#" .. sign.text .. "%*") or " ",
+    sign and ("%#" .. (sign.texthl or "DiagnosticInfo") .. "#" .. sign.text .. "%*") or "  ",
     [[%=]],
     nu .. " ",
     git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
