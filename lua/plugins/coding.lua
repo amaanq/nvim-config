bc561205fdc5012a99af5061a49cf46564e764d6
Amaan Qureshi <amaanq12@gmail.com>
2023-07-09 00:13:09 -0400

    feat: add codeql.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -384,4 +384,49 @@ return {
       },
     },
   },
+
+  {
+    "pwntester/codeql.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "v1.*",
+        opts = {
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            bo = {
+              filetype = {
+                "codeql_panel",
+                "codeql_explorer",
+                "qf",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "notify",
+                "NvimTree",
+                "neo-tree",
+              },
+              buftype = { "terminal" },
+            },
+          },
+          current_win_hl_color = "#e35e4f",
+          other_win_hl_color = "#44cc41",
+        },
+      },
+    },
+    opts = {
+      additional_packs = {
+        "/opt/codeql",
+      },
+    },
+    cmd = { "QL" },
+  },
+
+  {
+    "indent-blankline.nvim",
+    dev = true,
+  },
 }
