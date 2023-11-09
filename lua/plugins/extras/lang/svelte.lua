4f995a53421eca8ea9e4fb6b53b7b7d286d56737
Amaan Qureshi <amaanq12@gmail.com>
2023-11-09 04:22:39 -0500

    chore: updates

--- lua/plugins/extras/lang/svelte.lua
+++ lua/plugins/extras/lang/svelte.lua
@@ -4,20 +4,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "php" })
+        vim.list_extend(opts.ensure_installed, { "svelte" })
       end
     end,
   },
 
-  -- Correctly setup lspconfig for PHP
+  -- Correctly setup lspconfig for Svelte
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         -- Ensure mason installs the server
-        intelephense = {
-          root_dir = require("lspconfig").util.root_pattern("composer.json", ".git", "*.php"),
-        },
+        svelte = {},
       },
     },
   },
--- lua/plugins/extras/lang/verilog.lua
+++ lua/plugins/extras/lang/verilog.lua
@@ -2,6 +2,16 @@ return {
   {
     "mfussenegger/nvim-lint",
     optional = true,
+    init = function()
+      require("lint").linters.verilator.args = {
+        "-sv",
+        "-Wall",
+        "--bbox-sys",
+        "--bbox-unsup",
+        "--lint-only",
+        "--timing=22",
+      }
+    end,
     opts = {
       linters_by_ft = {
         verilog = { "verilator" },
