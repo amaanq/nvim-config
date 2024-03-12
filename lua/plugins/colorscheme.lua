47dd3659de3bc9b48c6c4d1b43877275311e8bd2
Amaan Qureshi <amaanq12@gmail.com>
2024-03-12 03:28:03 -0400

    feat(colorscheme): update highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -167,28 +167,43 @@ return {
           -- Smali
           hl["@parameter.builtin"] = { fg = "#efc890" }
 
+          -- C#
+          hl["@lsp.type.fieldName.cs"] = { link = "@field" }
+
+          hl["@character.printf"] = { link = "@type" }
+
           -- Rust
-          hl["@lsp.typemod.function.defaultLibrary.rust"] = { link = "@function" }
-          hl["@lsp.typemod.method.defaultLibrary.rust"] = { link = "@method" }
           hl["@lsp.type.selfTypeKeyword.rust"] = { link = "@variable.builtin" }
+          hl["@lsp.type.constantName"] = { link = "@constant" }
           hl["@lsp.type.decorator.rust"] = { link = "@attribute" }
           hl["@lsp.type.deriveHelper.rust"] = { link = "@attribute" }
+          hl["@lsp.type.extensionMethodName"] = { link = "@lsp.type.interface" }
           hl["@lsp.type.generic.rust"] = { link = "@variable" }
           hl["@lsp.type.formatSpecifier.rust"] = { link = "@punctuation.special" }
           hl["@lsp.type.variable.rust"] = { link = "@variable" }
           hl["@lsp.type.escapeSequence"] = { link = "@string.escape" }
+          hl["@lsp.type.stringEscapeCharacter"] = { link = "@string.escape" }
           hl["@lsp.type.selfKeyword"] = { link = "@variable.builtin" }
-          hl["@lsp.typemod.variable.constant.rust"] = { link = "@constant" }
-          hl["@lsp.typemod.keyword.async.rust"] = { link = "@keyword.coroutine" }
           hl["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" }
           hl["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" }
+          hl["@lsp.typemod.event.static"] = { link = "@lsp.type.interface" }
+          hl["@lsp.typemod.function.defaultLibrary.rust"] = { link = "@function.builtin" }
+          hl["@lsp.typemod.keyword.async"] = { link = "@keyword.coroutine" }
+          hl["@lsp.typemod.method.defaultLibrary.rust"] = { link = "@function.builtin" }
+          hl["@lsp.typemod.method.trait"] = { link = "@lsp.type.interface" }
+          hl["@lsp.typemod.variable.constant.rust"] = { link = "@constant" }
+
           -- why are these so bad?
           hl["@lsp.type.keyword.cs"] = {}
           hl["@lsp.type.keyword.go"] = {}
-          hl["@lsp.type.keyword.rust"] = {}
+          -- hl["@lsp.type.keyword.rust"] = {}
+          hl["@lsp.type.generic.rust"] = {}
           hl["@lsp.type.keyword.zig"] = {}
           hl["@lsp.type.type.zig"] = {}
           hl["@lsp.typemod.keyword.injected"] = { link = "@keyword" }
+
+          -- Rust doesn't really need strings
+          hl["@lsp.type.string.rust"] = {}
 
           hl["@lsp.type.operator"] = { link = "@operator" }
         end,
