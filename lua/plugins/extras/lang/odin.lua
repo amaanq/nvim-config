e75f8a5fdf08039419b5b9f0fe5598e0c14e3ed3
Amaan Qureshi <amaanq12@gmail.com>
2024-03-12 03:22:33 -0400

    feat: add Odin extra

---
+++ lua/plugins/extras/lang/odin.lua
@@ -0,0 +1,22 @@
+return {
+  -- Add Odin to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "odin" })
+      end
+    end,
+  },
+
+  -- Correctly setup lspconfig for Odin
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        ols = {},
+      },
+    },
+  },
+}
