35d6fe07a81d0f8951b709f2030fe32ae71aa8f9
Amaan Qureshi <amaanq12@gmail.com>
2024-04-10 10:26:40 -0400

    feat: move to native snippets

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -91,6 +91,7 @@ return {
           augend.date.alias["%Y/%m/%d"],
           augend.constant.alias.bool,
           augend.constant.new({ elements = { "let", "const" } }),
+          augend.constant.new({ elements = { "True", "False" } }),
           augend.semver.alias.semver,
         },
       })
@@ -98,17 +99,17 @@ return {
   },
 
   -- Supertab
-  {
-    "L3MON4D3/LuaSnip",
-    opts = {
-      history = true,
-      region_check_events = "InsertEnter",
-      delete_check_events = "TextChanged",
-    },
-    keys = function()
-      return {}
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   opts = {
+  --     history = true,
+  --     region_check_events = "InsertEnter",
+  --     delete_check_events = "TextChanged",
+  --   },
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
 
   {
     "nvim-cmp",
@@ -124,13 +125,13 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      -- local has_words_before = function()
+      --   unpack = unpack or table.unpack
+      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
 
-      local luasnip = require("luasnip")
+      -- local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       table.insert(opts.sources, { name = "git" })
@@ -139,31 +140,31 @@ return {
       -- table.insert(opts.sources, { name = "calc" })
       -- table.insert(opts.sources, { name = "spell" })
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.confirm({ select = true })
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
+      -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      --   ["<Tab>"] = cmp.mapping(function(fallback)
+      --     if cmp.visible() then
+      --       -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+      --       cmp.confirm({ select = true })
+      --     -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+      --     -- this way you will only jump inside the snippet region
+      --     elseif luasnip.expand_or_locally_jumpable() then
+      --       luasnip.expand_or_jump()
+      --     elseif has_words_before() then
+      --       cmp.complete()
+      --     else
+      --       fallback()
+      --     end
+      --   end, { "i", "s" }),
+      --   ["<S-Tab>"] = cmp.mapping(function(fallback)
+      --     if cmp.visible() then
+      --       cmp.select_prev_item()
+      --     elseif luasnip.jumpable(-1) then
+      --       luasnip.jump(-1)
+      --     else
+      --       fallback()
+      --     end
+      --   end, { "i", "s" }),
+      -- })
 
       opts.window = {
         completion = {
@@ -187,6 +188,21 @@ return {
         },
       })
 
+      -- cmp.setup.cmdline(":", {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = cmp.config.sources({
+      --     { name = "path" },
+      --   }, {
+      --     {
+      --       name = "cmdline",
+      --       option = {
+      --         ignore_cmds = { "Man", "!" },
+      --       },
+      --     },
+      --   }),
+      -- })
+
+      -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 
