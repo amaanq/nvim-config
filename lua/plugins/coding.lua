835212398adb9e864d2dbd309cb3ea2ad8d89207
Amaan Qureshi <amaanq12@gmail.com>
2024-07-26 21:39:39 -0400

    feat(treesj): 200 max join length

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -124,7 +124,7 @@ return {
     keys = {
       { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
-    opts = { use_default_keymaps = false, max_join_length = 150 },
+    opts = { use_default_keymaps = false, max_join_length = 200 },
   },
 
   {
