44cee12d3c6eb8b9403dd035df230fb9773e8516
Amaan Qureshi <amaanq12@gmail.com>
2024-12-01 22:27:01 -0500

    feat: add compiler.nvim

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -152,4 +152,25 @@ return {
       has_line_number = true,
     },
   },
+
+  {
+    "Zeioth/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+
+  {
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+      },
+    },
+  },
 }
