2b6f0cf08ce7e4f839d88a9dad6cc2dac97f9946
Amaan Qureshi <amaanq12@gmail.com>
2025-08-03 21:08:09 -0400

    fix: use custom svelte extra
    because no mason

---
+++ lua/plugins/extras/lang/svelte.lua
@@ -0,0 +1,75 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "svelte",
+      root = {
+        "svelte.config.js",
+        "svelte.config.mjs",
+        "svelte.config.cjs",
+      },
+    })
+  end,
+
+  -- depends on the typescript extra
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "svelte" } },
+  },
+
+  -- LSP Servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svelte = {
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = vim.fn.has("nvim-0.10") == 0 and { dynamicRegistration = true },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- Configure tsserver plugin
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local svelteserver_path = vim.fn.exepath("svelteserver")
+      if svelteserver_path ~= "" then
+        local plugin_location = svelteserver_path:gsub(
+          "/bin/svelteserver$",
+          "/lib/node_modules/svelte-language-server/dist/src/plugins/typescript"
+        )
+
+        LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+          {
+            name = "typescript-svelte-plugin",
+            location = plugin_location,
+            enableForWorkspaceTypeScriptVersions = true,
+          },
+        })
+      end
+    end,
+  },
+
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      if LazyVim.has_extra("formatting.prettier") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.svelte = { "prettier" }
+      end
+    end,
+  },
+}
