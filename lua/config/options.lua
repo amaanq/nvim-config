6d53443fb828911750176ec31d3f03af7a2b36da
Amaan Qureshi <contact@amaanq.com>
2025-09-16 03:14:54 -0400

    fix(rustacean): port lazyvim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -110,13 +110,54 @@ vim.g.rustaceanvim = {
     },
   },
   server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<leader>cR", function()
+        vim.cmd.RustLsp("codeAction")
+      end, { desc = "Code Action", buffer = bufnr })
+      vim.keymap.set("n", "<leader>dr", function()
+        vim.cmd.RustLsp("debuggables")
+      end, { desc = "Rust Debuggables", buffer = bufnr })
+    end,
     default_settings = {
+      -- rust-analyzer language server configuration
       ["rust-analyzer"] = {
-        cargo = {
-          targetDir = true,
-        },
         check = {
           targetDir = true,
+        },
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          buildScripts = {
+            enable = true,
+          },
+          targetDir = true,
+        },
+        -- Add clippy lints for Rust if using rust-analyzer
+        checkOnSave = diagnostics == "rust-analyzer",
+        -- Enable diagnostics if using rust-analyzer
+        diagnostics = {
+          enable = diagnostics == "rust-analyzer",
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ["async-trait"] = { "async_trait" },
+            ["napi-derive"] = { "napi" },
+            ["async-recursion"] = { "async_recursion" },
+          },
+        },
+        files = {
+          excludeDirs = {
+            ".direnv",
+            ".git",
+            ".github",
+            ".gitlab",
+            "bin",
+            "node_modules",
+            "target",
+            "venv",
+            ".venv",
+          },
         },
       },
     },
