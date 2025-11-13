839a8a86862faf0a814afc1220556e7c3cf9aaa6
Amaan Qureshi <git@amaanq.com>
2025-11-13 15:27:07 -0500

    nix: use latest statix version

--- flake.nix
+++ flake.nix
@@ -68,7 +68,18 @@
             nix = [
               pkgs.nixd
               pkgs.nixfmt-rfc-style
-              pkgs.statix
+              (pkgs.statix.overrideAttrs (_o: rec {
+                src = pkgs.fetchFromGitHub {
+                  owner = "oppiliappan";
+                  repo = "statix";
+                  rev = "43681f0da4bf1cc6ecd487ef0a5c6ad72e3397c7";
+                  hash = "sha256-LXvbkO/H+xscQsyHIo/QbNPw2EKqheuNjphdLfIZUv4=";
+                };
+                cargoDeps = pkgs.rustPlatform.importCargoLock {
+                  lockFile = src + "/Cargo.lock";
+                  allowBuiltinFetchGit = true;
+                };
+              }))
             ];
 
             python = [
