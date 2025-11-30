f7dc7e9cacbe0e6e4479e2504c16bf2aee07b842
Amaan Qureshi <git@amaanq.com>
2025-11-30 04:29:06 -0500

    plugin: add nvim-aosp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,10 @@
 return {
   { "fei6409/log-highlight.nvim", event = "BufRead *.log", opts = {} },
+
+  {
+    "amaanq/nvim-aosp",
+    config = function()
+      require("aosp").setup()
+    end,
+  },
 }
