2bd2bef60781848b4bb64859db3d3174f1906656
Amaan Qureshi <git@amaanq.com>
2026-01-24 15:16:50 -0500

    nvim: remove aosp plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,10 +1,3 @@
 return {
   { "fei6409/log-highlight.nvim", event = "BufRead *.log", opts = {} },
-
-  {
-    "amaanq/nvim-aosp",
-    config = function()
-      require("aosp").setup()
-    end,
-  },
 }
