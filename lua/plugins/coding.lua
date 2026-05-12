e17b0074ed5ef35672d2598b30ef2f2b85c4ac88
Amaan Qureshi <git@amaanq.com>
2026-05-12 19:32:28 -0400

    pairs: don't double backtick after a word

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,10 +1,18 @@
 return {
   {
+    "nvim-mini/mini.pairs",
+    opts = {
+      mappings = {
+        ["`"] = { neigh_pattern = "^[^%w_\\\\]" },
+      },
+    },
+  },
+
+  {
     "Wansmer/treesj",
     keys = {
       { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
     opts = { use_default_keymaps = false, max_join_length = 200 },
   },
-
 }
